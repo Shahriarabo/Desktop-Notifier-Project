6f5d60394385929f96b449a4548586a6c545eb19
Abdullah al shahriar <103172072+Shahriarabo@users.noreply.github.com>
2023-08-16 23:16:57 +0600

    Add files via upload

---
+++ Desktop-Notifier.py
@@ -0,0 +1,12 @@
+from win10toast import ToastNotifier
+
+toaster = ToastNotifier()
+
+toaster.show_toast("Learning Time ",
+                   "Hello Abdullah , It's time to learn Something New ",duration=5)
+toaster.show_toast("Learning Time ",
+                   "Hello Al Shahriar , It's time to learn Something New ",duration=5)
+
+
+
+
